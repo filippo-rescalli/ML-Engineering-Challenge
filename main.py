9b669e74d653a4756e26a1e2a01242569611d4ed
filipporescalli <10609906@polimi.it>
2024-10-27 21:14:27 +0100

    Docker Files and Subfeddit name-id mapping

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 import requests
 from textblob import TextBlob
 import json
@@ -28,37 +28,77 @@ def compute_sentiment(polarity_score):
     else:
         return 'Negative'
 
+def get_subfeddit_id(subfeddit_name):
+    api_url = 'http://feddit:8080/api/v1/subfeddits/?skip=0&limit=10'
+
+    try:
+        response = requests.get(api_url)
+
+        response.raise_for_status()
+        data = response.json()
+        
+        # Find subfeddit by name
+        subfeddits = data.get('subfeddits', [])
+        for subfeddit in subfeddits:
+            if subfeddit.get('title', '') == subfeddit_name:
+                return subfeddit.get("id", '')
+
+        # Raise exception if subfeddit is not found
+        raise HTTPException(status_code=404, detail="Subfeddit not found")
+
+    except requests.exceptions.RequestException as e:
+        raise HTTPException(status_code=500, detail="Failed to retrieve subfeddit ID")
+
+
 @app.get("/")
 def index():
     return {"details": "Hello World!"}
 
 @app.get("/comments", response_model=CommentResponse)
 async def get_latest_comments(
-        subfeddit_id: str,
+        subfeddit_name: str,
         start_date: datetime = None,
         end_date: datetime = None,
         sort_by_polarity: bool = False
     ):
+    """
+    Fetches and processes comments from a specified subfeddit.
+
+    Parameters:
+    - subfeddit_id: ID of the subfeddit to fetch comments from
+    - start_date: Filter to include comments only after this date
+    - end_date: Filter to include comments only before this date
+    - sort_by_polarity: Optionally sort comments by polarity score in descending order
+
+    Returns:
+    - A JSON response containing the latest 25 comments from a specific subfeddit
+    """
     limit=25
     comments_per_page=100
     all_comments = []
     skip = 0
 
+    subfeddit_id = get_subfeddit_id(subfeddit_name)
+
     while True:
         api_url = f'http://feddit:8080/api/v1/comments/?subfeddit_id={subfeddit_id}&skip={skip}&limit={comments_per_page}'
-        response = requests.get(api_url)
 
-        if response.status_code == 200:
+        try:
+            response = requests.get(api_url, timeout=5)
+            response.raise_for_status()
+
+            # Parse JSON response and extend comments list
             data = response.json()
             comments = data.get('comments', [])
             all_comments.extend(comments)
 
-            # Check if we received fewer comments than requested
+            # Exit if fewer comments than requested were received
             if len(comments) < comments_per_page:
-                break  
-        else:
-            print("Error fetching comments:", response.status_code, response.text)
-            break
+                break
+
+        except requests.exceptions.RequestException as e:
+            raise HTTPException(status_code=500, detail="Failed to retrieve comments")
+
 
         skip += comments_per_page
 
